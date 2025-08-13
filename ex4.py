6e11c836b6b1c4e614d90a0e4cf5efec6e549253
titima0369 <fatima.ezzahra.lahouani00@gmail.com>
2025-08-13 03:21:34 +0100

    Update ex4.py

---
+++ ex4.py
@@ -0,0 +1,8 @@
+
+user_height = float(input("Enter your height in centimeters: "))
+
+
+if user_height > 145:
+    print("You are tall enough to ride!")
+else:
+    print("You need to grow some more to ride.")
