f8cd7da2c32d96dabb627a24217fdaf2d49cfb65
titima0369 <fatima.ezzahra.lahouani00@gmail.com>
2025-08-13 03:08:57 +0100

    Update ex3.py

---
+++ ex3.py
@@ -0,0 +1,10 @@
+
+user_name = input("Hello! I'm Fatima Ezzahra  What's your name? ").strip().title()
+
+assistant_name = "Fatima Ezzahra"
+
+if user_name.lower() == assistant_name.lower():
+    print("\n WHOA! We're name twins! ")
+   
+else:
+    print(f"\n Lovely to meet you, {user_name}! ")
